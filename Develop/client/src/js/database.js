09b1a170e19131ff8ea4ea48aeca758152697379
keekerr <keeley.s.sprouse@gmail.com>
2023-03-16 22:11:06 -0400

    Added code to database.js file

--- Develop/client/src/js/database.js
+++ Develop/client/src/js/database.js
@@ -13,9 +13,25 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+  const db = await openDB('jate', 1);
+  const transaction = db.transaction('jate', 'readwrite');
+  const obStore = transaction.objectStore('jate');
+  const request = store.add({jate: content}); 
+  const result = await request;
 
-// TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+  console.log("successfully saved to databse", result);
+};
+  // TODO: Add logic for a method that gets all the content from the database
+export const getDb = async () => {
+  const db = await openDB('jate', 1);
+  const transaction = db.transaction('jate', 'readwrite');
+  const obStore = transaction.objectStore('jate');
+  const request = store.getALL();
+  const result = await request;
 
+  console.log("successfully received content", result);
+  
+
+};
 initdb();
